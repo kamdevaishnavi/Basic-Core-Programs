3a01fc55e80ae9e08578db2be1a9bc710af9c54f
vaishnavi-kamde <kamdevaishnavi1995@gmail.com>
2022-12-28 13:55:03 +0530

    ADDED: code for swap of number

---
+++ src/com/bridgelabz/basiccoreprogram/SwapNumber.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.basicprogram;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+        int num1 = 50;
+        int num2 = 100;
+        int temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("num1 = " +num1);
+        System.out.println("num2 = " +num2);
+    }
+}
+
